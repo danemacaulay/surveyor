4fa51f6399fe764723307d3304cc773a8edee8fd
Dane Macaulay <MacaulayD@DNB.com>
2016-03-17 09:51:57 -0500

    update results endpoint

--- server/resources/results.js
+++ server/resources/results.js
@@ -1,9 +1,8 @@
 'use strict';
 
 var path = require('path');
-var lodash = require('lodash');
+var crypto = require(path.resolve('./server/crypto'));
 
-var sequelize = require(path.resolve('./server/sequelize'));
 var Survey = require(path.resolve('./server/models/survey'));
 
 function buildQueryOptions() {
@@ -15,12 +14,22 @@ function buildQueryOptions() {
     return queryOptions;
 }
 
+// @TODO build out query for results
 module.exports =  function (req, res) {
+    var token = req.cookies.surveyor;
+
+    if (!token) {
+        res.status(401).json({});
+        return;
+    }
     var queryOptions = buildQueryOptions();
 
-    Survey.findAll(queryOptions).then(function (results) {
-        var survey = lodash.get(results, '[0].dataValues');
-        res.status(200).json(survey);
+    crypto.verify(token).then(function (data) {
+        console.log('data', data);
+    }).then(function () {
+        return Survey.findAll(queryOptions);
+    }).then(function (results) {
+        res.status(200).json(results);
     }).catch(function (err) {
         console.log(err);
         res.status(400).json({});
